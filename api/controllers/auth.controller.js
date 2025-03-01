0930e95bbe4728b977e029a586d7ef871de2f4e9
vithsan99 <vithusan091999@gmail.com>
2025-03-01 06:39:59 +0530

    sign in functionalities completed with returning twt token

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs'
+import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { errorHandler } from "../utils/error.js";
+
 export const signup = async (req,res,next) => {
     const {username,email,password} = req.body;
     const hashedPwd = bcryptjs.hashSync(password,10)
@@ -7,6 +10,23 @@ export const signup = async (req,res,next) => {
     try{
         await newUser.save();
         res.status(201).json('User created successfully')
+    } catch(err) {
+        next(err)
+    }
+}
+
+export const login = async (req,res,next) => {
+    const {email,password} = req.body;
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404,'User not found!'));
+        const validPasword = bcryptjs.compareSync(password,validUser.password)
+        if(!validPasword) return next(errorHandler(401,'Wrong credential!'));
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_TOKEN)
+        res
+        .cookie('access_token', token, {httpOnly:true})
+        .status(200)
+        .json(validUser);
     } catch(err) {
         next(err)
     }
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,77 @@
-import React from 'react'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignIn() {
+  const [formData,setFormData] = useState({})
+  const [loading,setLoading] = useState(false)
+  const [error,setError] = useState(null)
+  const navigate = useNavigate()
+  const handleChange = (e) =>{
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value
+      })
+    }
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        setLoading(true)
+        const res = await fetch('/api/auth/signin', 
+          {
+            method: 'POST',
+            headers:{
+              'Content-Type':'application/json'
+            },
+            body: JSON.stringify(formData)
+          }
+        );
+        const data = await res.json();
+        console.log(data);
+        if(data.success===false){
+        console.log('error from api ');
+          setError(data.message)
+          setLoading(false)
+          return;
+        } 
+        setLoading(false)
+        setError(null)
+        console.log('data from api');
+        navigate('/')
+      } catch (error) {
+        setLoading(false)
+        setError(error.message)
+        console.log('data client',error);
+      }
+      
+    }
+
   return (
-    <div>SignIn</div>
-  )
+    <div className="p-3 max-w-lg mx-auto ">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
+        <input
+          type="email"
+          placeholder="Email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+        />
+        <button disabled={loading} className="bg-slate-700 p-3 rounded-lg text-white hover:opacity-95 disabled:opacity-85 uppercase ">
+          {loading ? 'Loading...':'Sign In'}
+          </button>
+      </form>
+      <div className="flex gap-3 mt-2">
+        <p>Don't have an account?</p>
+        <Link to={'/sign-up'} className="text-blue-700">Sign Up</Link>
+      </div>
+      {error && <p className="text-red-700 mt-3">{error}</p>}
+    </div>
+  );
 }
